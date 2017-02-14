b211036ab8ab4ef44d65c96f566d6c0a24b28c17
Muthaheer <muthaheer1207@gmail.com>
2017-02-15 00:08:51 +0530

    initial commit

---
+++ include/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_SERVER', 'localhost');
+define('DB_NAME', 'fcm_chat');
+ 
+define("GOOGLE_API_KEY", "AIzaSyBICG2L1WtHu8mklMVgGMoTfzDupc_mjVQ");
+ 
+// push notification flags
+define('PUSH_FLAG_CHATROOM', 1);
+define('PUSH_FLAG_USER', 2);
+ 
+?>
